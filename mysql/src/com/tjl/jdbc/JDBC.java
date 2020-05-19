6234c82999ce25102b005aad2bddf42c793dd050
lunmm <1760290894@qq.com>
2020-05-19 23:23:17 +0800

    改一下

--- mysql/src/com/tjl/jdbc/JDBC.java
+++ mysql/src/com/tjl/jdbc/JDBC.java
@@ -1,17 +1,32 @@
 package com.tjl.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class JDBC {
 
 	public static void main(String[] args) {
-		//加载MySQL驱动
+//		加载MySQL驱动
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			System.out.println("OK");
-		} catch (ClassNotFoundException e) {
+			System.out.println("驱动加载成功");
+//		连接数据库,获得连接对象
+			Connection connection=DriverManager.getConnection("jdbc:mysql://121.199.78.141:3306/system", "root", "123456");
+			System.out.println("数据库连接成功");
+//		创建执行环境
+			Statement statement=connection.createStatement();
+			ResultSet result=statement.executeQuery("select * from user");
+			while(result.next()) {
+				System.out.println(result.getInt("id"));
+				System.out.println(result.getString("age"));
+			}
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			System.out.println("fail");
 			e.printStackTrace();
 		}
-	}
-
 }
+}
+
--- mysql/src/module-info.java
+++ mysql/src/module-info.java
@@ -1,2 +1,3 @@
 module mysql {
+	requires java.sql;
 }
